8551cd2f1c8f9f00eb7b0860754b9d44be61ff95
garrett griffey <garrettwgriffey@gmail.com>
2020-06-07 12:53:10 -0500

    updated html.js file paths

--- routes/html.js
+++ routes/html.js
@@ -2,13 +2,13 @@ const path = require("path");
 module.exports = function (app) {
     // routes
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname,"../public/index.html"));
+        res.sendFile(path.join(__dirname,"../public/html/index.html"));
     });
     app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname,"../public/stats.html"));
+        res.sendFile(path.join(__dirname,"../public/html/stats.html"));
     });
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname,"../public/exercise.html"));
+        res.sendFile(path.join(__dirname,"../public/html/exercise.html"));
     });
 
 };
